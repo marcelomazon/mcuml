232d528b2a3ee48db5836a35d85022f5dc2cbd20
Marcelo Maon <marcelomazon@gmail.com>
2019-03-26 23:36:45 -0300

    criacao projeto

---
+++ src/main/java/com/mazon/mcuml/McumlApplication.java
@@ -0,0 +1,13 @@
+package com.mazon.mcuml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McumlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McumlApplication.class, args);
+	}
+
+}
